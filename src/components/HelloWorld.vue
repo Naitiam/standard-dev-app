66fa5f6d89476b187b00c2f0eda56ccea9ca4554
maiiitian <mugibatake99@gmail.com>
2023-07-19 01:46:50 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from 'vue';
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 function foo() {
-console.log('Hello,foo') // 这里故意不缩进
+console.log('Hello,foo'); // 这里故意不缩进
 }
 foo();
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -22,9 +22,8 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Install
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +7,6 @@ export default defineConfig({
   server: {
     host: 'localhost',
     port: 8080,
-    open: true //自动打开浏览器
-  }
-})
+    open: true, //自动打开浏览器
+  },
+});
